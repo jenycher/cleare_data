02ff64976faf10d5abfdc56a724143b57621de4f
jenycher <168509289+jenycher@users.noreply.github.com>
2024-06-28 20:49:35 +0300

    add cl2

---
+++ clear2.py
@@ -0,0 +1,14 @@
+#Также можно удалять символы из списка при помощи метода pop и других.
+data = [
+
+    ['100', '200', '300'],
+    ['400', '500', '600']
+]
+# С сайта мы получаем именно списки.
+numbers = []
+
+for row in data:
+    for text in row:
+        number = int(text)
+        numbers.append(number)
+print(numbers)
